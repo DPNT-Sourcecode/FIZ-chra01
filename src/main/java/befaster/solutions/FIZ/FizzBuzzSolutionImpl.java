d6569222aead75853e7758e0d33c5c4ea917bb80
sbx_user1060 <sbx_user1060@ip-10-12-100-243.eu-west-2.compute.internal>
2018-08-12 13:45:44 +0000

    Sun Aug 12 13:45:44 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolutionImpl.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolutionImpl.java
@@ -1,18 +1,29 @@
 package befaster.solutions.FIZ;
 
+import java.util.List;
+import java.util.function.Function;
+
 public class FizzBuzzSolutionImpl implements FizzBuzzSolution {
+
+    Function<Integer, List<String>> transformer;
+
+    public FizzBuzzSolutionImpl(Function<Integer, List<String>> transformer) {
+        this.transformer = transformer;
+    }
 
     @Override
     public String fizzBuzz(Integer number) {
-        String output = String.valueOf(number);
-        if (isNumberDivisible(number, 15)) {
-            output = "fizz buzz";
-        } else if (isNumberDivisible(number, 3)) {
-            output = "fizz";
-        } else if (isNumberDivisible(number, 5)) {
-            output = "buzz";
-        }
-        return output;
+        List<String> transformedInput = transformer.apply(number);
+        return transformedInput.isEmpty() ? String.valueOf(number) : String.join(" ", transformedInput);
+//        String output = String.valueOf(number);
+//        if (isNumberDivisible(number, 15)) {
+//            output = "fizz buzz";
+//        } else if (isNumberDivisible(number, 3)) {
+//            output = "fizz";
+//        } else if (isNumberDivisible(number, 5)) {
+//            output = "buzz";
+//        }
+//        return output;
     }
 
 }

9f720b4380d5bdfddbfb7fe28944fd9cf1fb7f22
sbx_user1060 <sbx_user1060@ip-10-12-100-243.eu-west-2.compute.internal>
2018-08-12 14:03:44 +0000

    Sun Aug 12 14:03:44 UTC 2018

--- src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionImplTest.java
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionImplTest.java
@@ -1,6 +1,7 @@
 package befaster.solutions.FIZ;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -13,18 +14,28 @@ public class FizzBuzzSolutionImplTest {
 
     private static FizzBuzzSolution fizzBuzz;
 
-    @BeforeAll
-    public static void setUp() {
-        fizzBuzz = new FizzBuzzSolutionImpl(FizzBuzzUtil::fizzBuzzR1);
-    }
+//    @BeforeAll
+//    public static void setUp() {
+//        fizzBuzz = new FizzBuzzSolutionImpl(FizzBuzzUtil::fizzBuzzR1);
+//    }
 
+    @DisplayName("FizzBuzz R1")
     @ParameterizedTest(name = "Running fizzbuzz for {0} should return {1}")
-    @MethodSource("fizzBuzzData")
-    public void testFizzBuzz(Integer input, String output) {
+    @MethodSource("fizzBuzzDataR1")
+    public void testFizzBuzzR1(Integer input, String output) {
+        fizzBuzz = new FizzBuzzSolutionImpl(FizzBuzzUtil::fizzBuzzR1);
         assertEquals(output, fizzBuzz.fizzBuzz(input));
     }
 
-    private static Stream<Arguments> fizzBuzzData() {
+    @DisplayName("FizzBuzz R2")
+    @ParameterizedTest(name = "Running fizzbuzz R2 for {0} should return {1}")
+    @MethodSource("fizzBuzzDataR2")
+    public void testFizzBuzz(Integer input, String output) {
+        fizzBuzz = new FizzBuzzSolutionImpl(FizzBuzzUtil::fizzBuzzR2);
+        assertEquals(output, fizzBuzz.fizzBuzz(input));
+    }
+
+    private static Stream<Arguments> fizzBuzzDataR1() {
         return Stream.of(
                 Arguments.of(1, "1"),
                 Arguments.of(3, "fizz"),
@@ -33,6 +44,20 @@ public class FizzBuzzSolutionImplTest {
                 Arguments.of(10, "buzz"),
                 Arguments.of(15, "fizz buzz"),
                 Arguments.of(30, "fizz buzz")
+        );
+    }
+
+    private static Stream<Arguments> fizzBuzzDataR2() {
+        return Stream.of(
+                Arguments.of(1, "1"),
+                Arguments.of(3, "fizz"),
+                Arguments.of(13, "fizz"),
+                Arguments.of(9, "fizz"),
+                Arguments.of(5, "buzz"),
+                Arguments.of(15, "fizz buzz"),
+                Arguments.of(10, "buzz"),
+                Arguments.of(35, "fizz buzz"),
+                Arguments.of(53, "fizz buzz")
         );
     }
 

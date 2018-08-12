38d64bddfdd3bdc044ec9fb1c606b3fe8d015c26
sbx_user1060 <sbx_user1060@ip-10-12-100-243.eu-west-2.compute.internal>
2018-08-12 13:06:55 +0000

    Sun Aug 12 13:06:55 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolutionR3Impl.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolutionR3Impl.java
@@ -1,12 +1,5 @@
 package befaster.solutions.FIZ;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import static java.util.stream.Collectors.groupingBy;
-
 public class FizzBuzzSolutionR3Impl implements FizzBuzzSolution {
 
     FizzBuzzSolution baseSolution;
@@ -21,8 +14,10 @@ public class FizzBuzzSolutionR3Impl implements FizzBuzzSolution {
         String baseOutput = baseSolution.fizzBuzz(number);
 
         if (number > 10 && numberString.chars().filter(c -> c == numberString.charAt(0)).count() == numberString.length()) {
-            return numberString.equals(String.valueOf(number)) ? baseOutput
+            baseOutput = numberString.equals(String.valueOf(number)) ? "deluxe" : baseOutput + " deluxe";
         }
+
+        return baseOutput;
     }
 
 }
---
+++ src/test/java/befaster/solutions/FIZ/FizzBuxxSolutionR3ImplTest.java
@@ -0,0 +1,41 @@
+package befaster.solutions.FIZ;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FizzBuxxSolutionR3ImplTest {
+    private static FizzBuzzSolution fizzBuzz;
+
+    @BeforeAll
+    public static void setUp() {
+        fizzBuzz = new FizzBuzzSolutionR3Impl(new FizzBuzzSolutionR2Impl());
+    }
+
+    @ParameterizedTest(name = "Running fizzbuzz for {0} should return {1}")
+    @MethodSource("fizzBuzzData")
+    public void testFizzBuzz(Integer input, String output) {
+        assertEquals(output, fizzBuzz.fizzBuzz(input));
+    }
+
+    private static Stream<Arguments> fizzBuzzData() {
+        return Stream.of(
+                Arguments.of(1, "1"),
+                Arguments.of(3, "fizz"),
+                Arguments.of(13, "fizz"),
+                Arguments.of(9, "fizz"),
+                Arguments.of(5, "buzz"),
+                Arguments.of(15, "fizz buzz"),
+                Arguments.of(10, "buzz"),
+                Arguments.of(35, "fizz buzz"),
+                Arguments.of(53, "fizz buzz"),
+                Arguments.of(33, "fizz deluxe"),
+                Arguments.of(11, "deluxe")
+        );
+    }
+}

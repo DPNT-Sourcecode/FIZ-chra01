e6e506f2db91dd98d4eaf23c7820ba60bbfa748a
sbx_user1060 <sbx_user1060@ip-10-12-100-243.eu-west-2.compute.internal>
2018-08-12 12:42:55 +0000

    Sun Aug 12 12:42:55 UTC 2018

---
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolutionRefinedImpl.java
@@ -0,0 +1,15 @@
+package befaster.solutions.FIZ;
+
+public class FizzBuzzSolutionRefinedImpl {
+
+    FizzBuzzSolution baseSolution;
+
+    public FizzBuzzSolutionRefinedImpl(FizzBuzzSolution baseSolution) {
+        this.baseSolution = baseSolution;
+    }
+    
+    @Override
+    public String fizzBuzz(Integer number) {
+
+    }
+}
--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolutionUpperImpl.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolutionUpperImpl.java
@@ -1,4 +1,31 @@
 package befaster.solutions.FIZ;
 
-public class FizzBuzzSolutionUpperImpl {
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzzSolutionUpperImpl implements FizzBuzzSolution {
+
+    FizzBuzzSolution baseSolution;
+
+    public FizzBuzzSolutionUpperImpl(FizzBuzzSolution baseSolution) {
+        this.baseSolution = baseSolution;
+    }
+
+    @Override
+    public String fizzBuzz(Integer number) {
+        String numberString = String.valueOf(number);
+        String baseOutput =
+
+        List<String> outputList = new ArrayList<>();
+
+        if (isNumberDivisible(number, 3) || numberString.contains("3")) {
+            outputList.add("fizz");
+        }
+        if (isNumberDivisible(number, 5) || numberString.contains("5")) {
+            outputList.add("buzz");
+        }
+        if (outputList.isEmpty()) outputList.add(numberString);
+        return String.join(" ", outputList);
+    }
+
 }

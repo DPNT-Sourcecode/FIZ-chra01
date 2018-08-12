91a76c2ef4e15ee79ec146e23e780a62944d0138
sbx_user1060 <sbx_user1060@ip-10-12-100-243.eu-west-2.compute.internal>
2018-08-12 11:53:46 +0000

    Sun Aug 12 11:53:46 UTC 2018

--- src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
+++ src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
@@ -1,4 +1,21 @@
 package befaster.solutions.HLO;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class HelloSolutionTest {
+
+    private static HelloSolution hello;
+
+    @BeforeAll
+    public static void setUp() {
+        hello = new HelloSolution();
+    }
+
+    @Test
+    public void testHello() {
+        assertEquals("hello", hello.hello("test"));
+    }
 }

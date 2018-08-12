920897f7cdbd4ae5030322fe39e7343af8547c2b
sbx_user1060 <sbx_user1060@ip-10-12-100-243.eu-west-2.compute.internal>
2018-08-12 12:05:40 +0000

    Sun Aug 12 12:05:40 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -5,7 +5,12 @@ import befaster.runner.SolutionNotImplementedException;
 public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer number) {
-        throw new SolutionNotImplementedException();
+        if (number % 15 == 0) {
+            return "fizz buzz";
+        }
+        if (number % 3) ==0
     }
+
+    private isNumberDivisible(int dividend, int divisor)
 
 }
--- src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
@@ -1,4 +1,38 @@
 package befaster.solutions.FIZ;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzSolutionTest {
+
+    private static FizzBuzzSolution fizzBuzz;
+
+    @BeforeAll
+    public static void setUp() {
+        fizzBuzz = new FizzBuzzSolution();
+    }
+
+    @ParameterizedTest(name = "Running fizzbuzz for {0} should return {1}")
+    @MethodSource("fizzBuzzData")
+    public void testFizzBuzz(Integer input, String output) {
+        assertEquals(output, fizzBuzz.fizzBuzz(input));
+    }
+
+    private static Stream<Arguments> createFizzBuzz() {
+        return Stream.of(
+                Arguments.of(1, "1"),
+                Arguments.of(3, "fizz"),
+                Arguments.of(5, "buzz"),
+                Arguments.of(15, "fizz buzz")
+        );
+    }
+
 }

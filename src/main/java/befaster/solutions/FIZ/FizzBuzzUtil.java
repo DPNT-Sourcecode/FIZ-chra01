fbb498fd3b2ceec73eb2163c15d7212b48abf4a7
sbx_user1060 <sbx_user1060@ip-10-12-100-243.eu-west-2.compute.internal>
2018-08-12 13:36:44 +0000

    Sun Aug 12 13:36:44 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzUtil.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzUtil.java
@@ -6,19 +6,30 @@ import java.util.function.Function;
 
 public class FizzBuzzUtil {
 
-
-
-    public String fizzBuzzR1(Integer number) {
+    public List<String> fizzBuzzR1(Integer number) {
         List<String> outputList = new ArrayList<>();
-        String output = String.valueOf(number);
-        if (isNumberDivisible(number, 15)) {
-            output = "fizz buzz";
-        } else if (isNumberDivisible(number, 3)) {
-            output = "fizz";
+        if (isNumberDivisible(number, 3)) {
+            outputList.add("fizz");
         } else if (isNumberDivisible(number, 5)) {
-            output = "buzz";
+            outputList.add("buzz");
         }
+        return outputList;
     }
+
+    public List<String> fizzBuzzR2(Integer number) {
+        List<String> outputList = new ArrayList<>();
+        String numberString = String.valueOf(number);
+        if (isNumberDivisible(number, 3) || numberString.contains("3")) {
+            outputList.add("fizz");
+        }
+        if (isNumberDivisible(number, 5) || numberString.contains("5")) {
+            outputList.add("buzz");
+        }
+        if (outputList.isEmpty()) outputList.add(numberString);
+        return String.join(" ", outputList);
+    }
+
+
 
     private boolean isNumberDivisible(int dividend, int divisor) {
         return dividend % divisor == 0;
